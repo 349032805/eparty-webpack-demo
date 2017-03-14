abbfb0bd5a4e3b62003e72bc52996144904ffe32
349032805@qq.com <349032805@qq.com>
2017-03-14 16:33:23 +0800

    git commit

--- app/index.js
+++ app/index.js
@@ -1,15 +1,15 @@
-import './style/common.scss';
-import './style/nav.scss';
-import './style/main.scss';
+import './styles/common.scss';
+import './styles/nav.scss';
+import './styles/main.scss';
 
 import 'angular';
 import 'angular-animate';
 import 'angular-ui-router';
 import 'angular-ui-bootstrap';
 
-import routerconfig from './js/routers/router.js';
-import controllerModule from './js/controllers/controller.js'
-export default angular.module('epartyApp',['ui.router','ui.bootstrap','ngAnimate',controllerModule])
+import routerconfig from './scripts/routers/router.js';
+import controllerModule from './scripts/controllers/controller.js'
+export default angular.module('epartyApp',['ui.router','ngAnimate',controllerModule])
 .config(routerconfig)
 .controller('appCtrl', function ($scope,$state,$stateParams) {
   $scope.$state=$state;
