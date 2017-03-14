1922ffc4c7b4a58ecda79c6ff56516e88ba9ca7b
349032805@qq.com <349032805@qq.com>
2017-03-14 19:38:52 +0800

    git commit

--- app/scripts/routers/router.js
+++ app/scripts/routers/router.js
@@ -7,18 +7,15 @@ var router=function($urlRouterProvider,$stateProvider){
         })
         .state("test", {
             url: "/test",
-            templateUrl: "/views/test.html",
-            controller: 'TestCtrl'
+            templateUrl: "/views/test.html"
         })
          .state("waiterRanking", {
             url: "/waiterRanking",
-            templateUrl: "/views/waiterRanking.html",
-            controller: 'WaiterRankingCtrl'
+            templateUrl: "/views/waiterRanking.html"
         })
           .state("hasOrder", {
             url: "/hasOrder",
-            templateUrl: "/views/hasOrder.html",
-            controller: 'HasOrderCtrl'
+            templateUrl: "/views/hasOrder.html"
         });
        
     $urlRouterProvider.otherwise("/index");
