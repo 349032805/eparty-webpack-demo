bcea7a1267f1054b02c58727f14b41c2a0cfccd3
349032805@qq.com <349032805@qq.com>
2017-03-15 11:48:33 +0800

    git commit

--- app/scripts/controllers/controller.js
+++ app/scripts/controllers/controller.js
@@ -1,10 +1,9 @@
-import angular from 'angular'
+
 import UserListsCtrl from './userLists.js'
 import ModalDemoCtrl from './networkManagerCtrl.js'
 import WaiterRankingCtrl from './waiterRanking.js'
 import rainbowDirective from '../directives/rainbow.js'
-export default angular.module('controllerModule',[])
-.controller('userListsCtrl',UserListsCtrl)
-.controller('ModalDemoCtrl',ModalDemoCtrl)
+
+export default angular.module('controllerModule',[rainbowDirective])
 .controller('WaiterRankingCtrl',WaiterRankingCtrl)
 .name
--- app/scripts/directives/rainbow.js
+++ app/scripts/directives/rainbow.js
@@ -1,16 +1,16 @@
   'use strict';
 
-  angular.module('epartyApp')
-    .directive('rainbow', function () {
-      return {
-        restrict: 'E',
-      	template: '<div>这是自定义指令!</div>',
-      	 // templateUrl: 'views/xxx.html'
-      	link:postLink
-      };
+export default angular.module('rainbowDirective',[])
+  .directive('rainbow', function () {
+    return {
+      restrict: 'E',
+    	template: '<div>这是自定义指令!</div>',
+    	 // templateUrl: 'views/xxx.html'
+    	link:postLink
+    };
 
-	     function postLink(scope, element, attrs) {
-	      element.css("color", attrs.color);
-	    }
+     function postLink(scope, element, attrs) {
+      element.css("color", attrs.color);
+    }
 
-    });
+}).name
