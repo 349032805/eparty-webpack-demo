758e123ddb9cd12c496619115390733d4e1738ca
349032805@qq.com <349032805@qq.com>
2017-03-15 14:33:38 +0800

    git commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,22 @@
 
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 var path = require('path');
+var node_modules = path.resolve(__dirname, 'node_modules');
 
 module.exports={
-    entry:'./app/index.js',
+    // entry:'./app/index.js',
+    entry: __dirname + "/app/index.js",
     output:{
         path:__dirname + '/app',
         filename:'bundle.js'
     },
     module:{
         loaders:[
-            {
-                test:/\.css$/,
-                loader:'style!css'
-            },
-            {
-                test:/\.scss/,
-                loader:'style!css!sass'
-            },
-            {
-                test:/\.js$/,
-                exclude:/(node_modules)/,
-                loader:'babel',
-                query:{
-                    presets:['latest']
-                }
-            },
-            {
-                test:/.(png|jpg|svg)$/, 
-                loader: "url?limit=50000"
-            }
+          { test: /\.css$/, loader: 'style!css' },
+          { test: /\.scss$/, loader: 'style!css!sass' },
+          { test: /\.js$/, loader:'babel', query:{ presets:['latest']}, exclude: path.resolve(__dirname, node_modules) },
+          { test: /\.(png|jpg|gif)$/, loader: 'url?limit=8192' },
+          { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/, loader: 'file-loader' }
         ]
     },
 
