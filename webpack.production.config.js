b731cebcfebf206c39f8af027a1727bec678e191
349032805@qq.com <349032805@qq.com>
2017-03-16 18:16:25 +0800

    git commit

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -11,57 +11,53 @@ var ENV = process.env.NODE_ENV || 'development';
 module.exports = {
   // devtool: 'source-map',
   devtool: 'cheap-source-map',
-  entry: [
-    path.resolve(__dirname, 'app/index.js'),
-  ],
+  // entry: [
+  //   path.resolve(__dirname, 'app/index.js'),
+  // ],
+  entry:'./app/index.js',
   output: {
     path: __dirname + '/build',
-    publicPath: '/',
-    filename: 'bundle-[hash:6].js'
+    // publicPath: '/',
+    filename: "scripts/[name]-[chunkhash:8].js"
   },
-  // module: {
-  //   loaders:[
-  //     { test: /\.css$/, loader: 'style!css' },
-  //     { test: /\.scss$/, loader: 'style!css!sass' },
-  //     { test: /\.js$/, loader:'babel', query:{ presets:['latest']}, exclude: path.resolve(__dirname, node_modules) },
-  //     // { test: /\.(png|jpg|gif)$/, loader: 'url?limit=8192' },
-  //     {test: /\.(png|jpg|gif)$/,loader: 'url?limit=8192,name=/images/[name]-[hash:6].[ext]'},
-  //     { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/, loader: 'file-loader' }
-  //   ]
-  // },
-
-   module: {
-        loaders: [{
+  module:{
+    loaders:[
+            {
+                test:/\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
+            {
+                test:/\.scss$/,
+                loader: ExtractTextPlugin.extract('style', 'css!sass',{
+                    publicPath: '../'
+                })
+            },
+             { test: /\.js$/, loader:'babel', query:{ presets:['latest']}, exclude: /node_modules/ },
+            {
+                test:/\.(png|jpg|gif|woff|woff2|ttf|eot|svg|swf)$/,
+                loader: "url-loader?limit=8192&name=/images/[name]-[hash:8].[ext]"
+            },
+            {
+    　　　　　　 test: /\.(htm|html)$/i,
+    　　　　　　 loader: 'html-withimg-loader'
+    　　　　 },
+            {
                 test: /\.html$/,
-                loader: 'raw'
-            }, {
-                test: /\.(png|jpg|gif)$/,
-                loader: 'url?limit=8192,name=/img/[name].[hash:6].[ext]'
-            }, {
-                test: /\.(woff|woff2|eot|ttf|svg)(\?.*$|$)/,
-                loader: 'url-loader?importLoaders=1&limit=1000&name=/fonts/[name].[ext]'
-            }, {
-                test: /\.css$/,
-                loaders: ['style', 'css'],
-            }, 
-            // { //外链
-            //     test: /\.scss$/,
-            //     loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader!postcss-loader")
-            // }
-            {  //内联
-                test: /\.scss$/,
-                loaders: ['style', 'css', 'sass','postcss']
+                loader: "html-loader"
             }
         ]
+        
     },
   plugins: [
     new CleanWebpackPlugin('build', {
       verbose: true,
       dry: false
     }),
+   new ExtractTextPlugin("styles/[name]-[contenthash:8].css"),
    new HtmlWebpackPlugin({
       template: __dirname + '/app/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
+      chunkhash:true
     }),
     // new webpack.DefinePlugin({
     //   'process.env':{
